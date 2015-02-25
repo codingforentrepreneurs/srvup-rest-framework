c42a3fe32d30bc75460544117f5c9ccb0dfd4da5
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-02-24 16:40:21 -0800

    Basic Foreign Key Usage

--- src/videos/serializers.py
+++ src/videos/serializers.py
@@ -5,10 +5,14 @@ from rest_framework import routers, serializers, viewsets, permissions
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 
-from .models import Video
+from .models import Video, Category
 
 
 class VideoSerializer(serializers.HyperlinkedModelSerializer):
+	# category_url = serializers.CharField(source='category.get_absolute_url', read_only=True)
+	category_title = serializers.CharField(source='category.title', read_only=True)
+	category_image = serializers.CharField(source='category.get_image_url', read_only=True)
+	category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
 	class Meta:
 		model = Video
 		fields = [
@@ -19,6 +23,10 @@ class VideoSerializer(serializers.HyperlinkedModelSerializer):
 			'embed_code',
 			'share_message',
 			'timestamp',
+			"category",
+			"category_image",
+			"category_title",
+			# "category_url",
 		]
 
 
