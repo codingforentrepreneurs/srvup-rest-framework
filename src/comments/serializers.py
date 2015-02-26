ffa96b08bd98a96d0e634365bc16ad4e8cd59cab
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-02-25 16:41:21 -0800

    Serializer Method Field

--- src/comments/serializers.py
+++ src/comments/serializers.py
@@ -14,14 +14,35 @@ from .models import  Comment
 
 User= get_user_model()
 
+class ChildCommentSerializer(serializers.HyperlinkedModelSerializer):
+	user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+	class Meta:
+		model = Comment
+		fields = [
+			'id',
+			"user",
+			'text',
+		]
+
+
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
 	user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+	children = serializers.SerializerMethodField(read_only=True)
+
+	def get_children(self, instance):
+		#queryset = instance.get_children()
+		queryset = Comment.objects.filter(parent__pk=instance.pk)
+		serializer = ChildCommentSerializer(queryset, context={"request": instance}, many=True)
+		return serializer.data
+
+
 	class Meta:
 		model = Comment
 		fields = [
 			"url",
 			'id',
-			"parent",
+			"children",
+			#"parent",
 			"user",
 			# 'video',
 			'text',
