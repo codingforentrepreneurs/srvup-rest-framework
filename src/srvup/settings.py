50fa34c554df47c66dcb65a34eb791540ab0c4d3
Justin Mitchel <jmitchel3@gmail.com>
2015-06-14 10:08:21 -0700

    Update Django Template Settings for 1.8

--- src/srvup/settings.py
+++ src/srvup/settings.py
@@ -125,9 +125,35 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static_root")
 
+
+'''
+Django versions 1.7 and below.
+
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, "templates"),
 )
+
+
+'''
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, "templates"),],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+
 
 
 MEDIA_URL = '/media/'
